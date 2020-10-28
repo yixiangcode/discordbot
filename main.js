1f1b04348aa5bb86ff40946f7a1b2a11881c138d
YI XIANG <58964880+yixianggg@users.noreply.github.com>
2020-10-28 22:40:41 +0800

    First

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const token = 'NzcwMjY3NTEwOTU5ODk4NjI1.X5bFhQ.ngJSg0pTO6RKRtdy6GlDVPdLGmM';
+//const token = 'NzcwMjY3NTEwOTU5ODk4NjI1.X5bFhQ.ngJSg0pTO6RKRtdy6GlDVPdLGmM';
 
 client.once('ready',()=>{
     console.log('你的小可爱已上线哟~');
