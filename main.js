201eacf9d5c4e03299cd899e3d0ef052a16dff32
YI XIANG <58964880+yixianggg@users.noreply.github.com>
2020-10-29 22:33:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -118,10 +118,6 @@ client.on('message', message=>{
         case 'q':
             message.reply('😘')
             break;
-        case 'image':
-            image(message);
-
-            break;
             /*
         case '闭嘴':
             message.reply('OK😥');
@@ -139,35 +135,4 @@ client.on('message', message=>{
             */
     }
 })
-
-function image(message){
-    
-    var options = {
-        url:"http://results.dogpile.com/serp?qc=images&q" + "minecraft",
-        method:"GET",
-        headers:{
-            "Accept":"text/html",
-            "User-Agent":"Chrome"
-        }
-    };
-}
-
-Request(options,function(error,response,responseBody){
-    if(error){
-        return;
-    }
-
-    $=cheerio.load(responseBody);
-
-    var links = $(".image a.link");
-
-    var urls=new Array(links.length).fill(0).map((v,i)=>links.eq(i).attr("href"));
-
-    console.log(urls);
-    if(!urls.length){
-        return;
-    }
-    message.channel.send( urls[Math.floor(Math.random()*urls.length)]+" "+message.guild.members.random());
-});
-
 client.login(process.env.token);
