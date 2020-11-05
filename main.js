8fb8b5a38db01d8b95234148d31b3bab0000948b
YI XIANG <58964880+yixianggg@users.noreply.github.com>
2020-11-05 12:56:32 +0800

    a

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const Levels = require('discord-xp');
 const canvacord = require("canvacord");
 const Commando = require('discord.js-commando')
 const path = require('path')
@@ -35,17 +36,26 @@ client.once('ready',()=>{
 });
 
 client.on("message", async (message) => {
-    if (message.author.client) return;
-    if (message.content === "!test") {
-        let avatar = message.author.displayAvatarURL({ dynamic: false, format: 'png' });
-        let image = await canvacord.Canvas.trigger(avatar);
-        let attachment = new Discord.MessageAttachment(image, "triggered.gif");
-        return message.channel.send(attachment);
-    }
-    if(message.content==="he"){
-        message.channel.send("dafug");
-    }
-});
+    const canvacord = require("canvacord");
+    const img = "https://cdn.discordapp.com/embed/avatars/0.png";
+ 
+    const userData = getDataSomehow();
+ 
+    const rank = new canvacord.Rank()
+        .setAvatar(img)
+        .setCurrentXP(userData.xp)
+        .setRequiredXP(userData.requiredXP)
+        .setStatus("dnd")
+        .setProgressBar("#FFFFFF", "COLOR")
+        .setUsername("Snowflake")
+        .setDiscriminator("0007");
+ 
+    rank.build()
+        .then(data => {
+            const attachment = new Discord.MessageAttachment(data, "RankCard.png");
+            message.channel.send(attachment);
+        });
+
 
 client.on('message',message=>{
     
