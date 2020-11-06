b0a91916d67dbeebaf8501eef8bff09c28f4b597
YI XIANG <58964880+yixianggg@users.noreply.github.com>
2020-11-06 11:50:20 +0800

    a

--- main.js
+++ main.js
@@ -281,7 +281,7 @@ client.on('message',message=>{
         case '电':
             
             console.log(message.author.username);
-            if(message.author.username==="K."||"KK."||"阿乐"){
+            if(message.author.username==="K."){
                 message.channel.send("Sorry! y²N is not longer support your username.");
             }else{
                 var embed = new Discord.MessageEmbed()
@@ -296,7 +296,7 @@ client.on('message',message=>{
             break;
         case 'time':
             console.log(message.author.username);
-            if(message.author.username==="K."||"KK."||"阿乐"){
+            if(message.author.username==="K."){
                 message.channel.send("Sorry! y²N is not longer support your username.");
             }else{
                 message.channel.send("上课时间\n```第一节：7.35am-8.15am\n第二节：8.20am-9.00am\n第三节：9.05am-9.45am\n第四节：9.50am-10.30am\n第五节：10.35am-11.15am\n第六节：11.20am-12.00pm\n第七节：12.20pm-1.00pm\n第八节：1.05pm-1.45```")
@@ -305,7 +305,7 @@ client.on('message',message=>{
         case 'clear':
             console.log(message.author.username);
             if(!args[1]) return message.reply('亲爱的要写几行哟~');
-            if(message.author.username==="K."||"KK."||"阿乐"){
+            if(message.author.username==="K."){
                 message.channel.send("Sorry! y²N is not longer support your username.");
             }else{
                 message.channel.bulkDelete(args[1]);
