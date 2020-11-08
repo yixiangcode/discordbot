485e2c2ecce994ea472bdc3844ac4a37db8f6ab1
YI XIANG <58964880+yixianggg@users.noreply.github.com>
2020-11-08 22:28:08 +0800

    a

--- main.js
+++ main.js
@@ -248,9 +248,9 @@ client.on('message',message=>{
                 message.channel.send(embed);
             }
             break;
-        case 'time':
+        case '时间':
             console.log(message.author.username);
-            if(message.author.username==="K."||message.author.username==="KK."||message.author.username==="阿乐"||message.author.username==="疯子"){
+            if(message.author.username==="K."){
                 message.channel.send("Sorry! y²N is not longer support your username.");
             }else{
                 message.channel.send("上课时间\n```第一节：7.35am-8.15am\n第二节：8.20am-9.00am\n第三节：9.05am-9.45am\n第四节：9.50am-10.30am\n第五节：10.35am-11.15am\n第六节：11.20am-12.00pm\n第七节：12.20pm-1.00pm\n第八节：1.05pm-1.45```")
