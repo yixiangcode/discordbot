075d97f8ae9838658a720134d62de22342079cf1
YI XIANG <58964880+yixianggg@users.noreply.github.com>
2020-11-12 14:14:10 +0800

    a

--- main.js
+++ main.js
@@ -290,8 +290,8 @@ client.on('message',message=>{
             if(!args[1]) return message.reply('是不会出喔🙄');
                 const messages = ["石头","剪刀","布"];
                 const random025 = messages[Math.floor(Math.random() * messages.length)];
-                v=Math.floor()
-                var s = 0;
+                var v = 1;
+                var s = 1;
                 let score = "Bot : "+v+" 【VS】 "+"Player : "+s;
                 let gameWin = false;
                 if(random025==="石头"){
