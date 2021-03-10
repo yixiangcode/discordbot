f24d3cc7576b68ab50fec23dc4110f0ed0e1bd28
YI XIANG <58964880+yixianggg@users.noreply.github.com>
2021-03-10 10:47:36 +0800

    a

--- main.js
+++ main.js
@@ -26,9 +26,12 @@ client.once('ready',()=>{
 
 client.on('ready',() => {
     var chn = client.channels.cache.get('770291696034381844');
-    var lamp = client.channels.cache.get('707515094568927295');
+    var lampchn = client.channels.cache.get('812227828362772521');
+
+
     //var chn = client.channels.cache.get('707515094568927295');
     var msgNumber = "802029057029832735";
+    var lamp = "819036967144259614"
     setInterval(() => {
         const date = new Date();
         //d=date.getDay();
@@ -75,39 +78,75 @@ client.on('ready',() => {
 
 
             if(s==="00"){
-                lamp.messages.fetch({around: "819032725960654868", limit: 1}).then(msg => {
+                lampchn.messages.fetch({around: "lamp", limit: 1}).then(msg => {
                     const fetchedMsg = msg.first();
-                    fetchedMsg.edit("🔴🟢🟢🟢🟢🟢");
+                    fetchedMsg.edit("🔴🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢");
+                });
+            }
+            if(s==="05"){
+                lampchn.messages.fetch({around: "lamp", limit: 1}).then(msg => {
+                    const fetchedMsg = msg.first();
+                    fetchedMsg.edit("🟢🔴🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢");
                 });
             }
             if(s==="10"){
-                lamp.messages.fetch({around: "819032725960654868", limit: 1}).then(msg => {
+                lampchn.messages.fetch({around: "lamp", limit: 1}).then(msg => {
                     const fetchedMsg = msg.first();
-                    fetchedMsg.edit("🟢🔴🟢🟢🟢🟢");
+                    fetchedMsg.edit("🟢🟢🔴🟢🟢🟢🟢🟢🟢🟢🟢🟢");
+                });
+            }
+            if(s==="15"){
+                lampchn.messages.fetch({around: "lamp", limit: 1}).then(msg => {
+                    const fetchedMsg = msg.first();
+                    fetchedMsg.edit("🟢🟢🟢🔴🟢🟢🟢🟢🟢🟢🟢🟢");
                 });
             }
             if(s==="20"){
-                lamp.messages.fetch({around: "819032725960654868", limit: 1}).then(msg => {
+                lampchn.messages.fetch({around: "lamp", limit: 1}).then(msg => {
                     const fetchedMsg = msg.first();
-                    fetchedMsg.edit("🟢🟢🔴🟢🟢🟢");
+                    fetchedMsg.edit("🟢🟢🟢🟢🔴🟢🟢🟢🟢🟢🟢🟢");
+                });
+            }
+            if(s==="25"){
+                lampchn.messages.fetch({around: "lamp", limit: 1}).then(msg => {
+                    const fetchedMsg = msg.first();
+                    fetchedMsg.edit("🟢🟢🟢🟢🟢🔴🟢🟢🟢🟢🟢🟢");
                 });
             }
             if(s==="30"){
-                lamp.messages.fetch({around: "819032725960654868", limit: 1}).then(msg => {
+                lampchn.messages.fetch({around: "lamp", limit: 1}).then(msg => {
                     const fetchedMsg = msg.first();
-                    fetchedMsg.edit("🟢🟢🟢🔴🟢🟢");
+                    fetchedMsg.edit("🟢🟢🟢🟢🟢🟢🔴🟢🟢🟢🟢🟢");
+                });
+            }
+            if(s==="35"){
+                lampchn.messages.fetch({around: "lamp", limit: 1}).then(msg => {
+                    const fetchedMsg = msg.first();
+                    fetchedMsg.edit("🟢🟢🟢🟢🟢🟢🟢🔴🟢🟢🟢🟢");
                 });
             }
             if(s==="40"){
-                lamp.messages.fetch({around: "819032725960654868", limit: 1}).then(msg => {
+                lampchn.messages.fetch({around: "lamp", limit: 1}).then(msg => {
                     const fetchedMsg = msg.first();
-                    fetchedMsg.edit("🟢🟢🟢🟢🔴🟢");
+                    fetchedMsg.edit("🟢🟢🟢🟢🟢🟢🟢🟢🔴🟢🟢🟢");
+                });
+            }
+            if(s==="45"){
+                lampchn.messages.fetch({around: "lamp", limit: 1}).then(msg => {
+                    const fetchedMsg = msg.first();
+                    fetchedMsg.edit("🟢🟢🟢🟢🟢🟢🟢🟢🟢🔴🟢🟢");
                 });
             }
             if(s==="50"){
-                lamp.messages.fetch({around: "819032725960654868", limit: 1}).then(msg => {
+                lampchn.messages.fetch({around: "lamp", limit: 1}).then(msg => {
                     const fetchedMsg = msg.first();
-                    fetchedMsg.edit("🟢🟢🟢🟢🟢🔴");
+                    fetchedMsg.edit("🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🔴🟢");
+                });
+            }
+            if(s==="55"){
+                lampchn.messages.fetch({around: "lamp", limit: 1}).then(msg => {
+                    const fetchedMsg = msg.first();
+                    fetchedMsg.edit("🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🟢🔴");
                 });
             }
 
