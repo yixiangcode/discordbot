1e4631b5a7a3e034746bdc2530ef8e2de7cb7cd2
YI XIANG <58964880+yixianggg@users.noreply.github.com>
2021-05-22 23:52:50 +0800

    a

--- main.js
+++ main.js
@@ -359,7 +359,7 @@ const PREFIX = '&';
 client.on('message',message=>{
 	const date = new Date(); // today
 	if(message.content==="jt"){
-		message.channel.send("**Server join Date: **" + date.author.joinedAt + "\n");
+		message.channel.send("**Server join Date: **" + date.join + "\n");
 	}
     if(message.content.startsWith ("write")){
         editedmessage= message.content.slice(6);
