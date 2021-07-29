bb83816e1ba6bbb35c4c89e1409eb01c84ab9bbd
YI XIANG <58964880+yixiangcode@users.noreply.github.com>
2021-07-29 22:40:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -445,7 +445,7 @@ client.on('message',message=>{
     }
 	
 	if(message.content ==="123" && message.author.username==="2302"){
-		for(var i=925519;i<1000000;i++){
+		for(var i=965384;i<1000000;i++){
 			message.channel.send(i+1);
 		}
         
