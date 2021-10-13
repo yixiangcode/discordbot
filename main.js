2428ad5cfe32824808d23ac2f466b7d9abe342f6
YI XIANG <yixiang972@gmail.com>
2021-10-13 16:24:57 +0800

    a

--- main.js
+++ main.js
@@ -9,6 +9,33 @@ const cron = require('cron');
 const moment = require('moment');
 const { MessageActionRow, MessageSelectMenu } = require('discord.js');
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageSelectMenu()
+					.setCustomId('select')
+					.setPlaceholder('Nothing selected')
+					.addOptions([
+						{
+							label: 'Select me',
+							description: 'This is a description',
+							value: 'first_option',
+						},
+						{
+							label: 'You can select me too',
+							description: 'This is also a description',
+							value: 'second_option',
+						},
+					]),
+			);
+
+		await interaction.reply({ content: 'Pong!', components: [row] });
+	}
+});
+
 const client = new Discord.Client();
 var version = '1.2';
 var servers = {};
